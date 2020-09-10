1bb268955cb41620cc5935ef7353eecb7bef1331
Muhaamd-Haseeb <64950545+Muhaamd-Haseeb@users.noreply.github.com>
2020-09-10 21:49:21 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,9 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-import './App.css';
 
-const useStyles = makeStyles((theme) => ({
-  formControl: {
-    margin: theme.spacing(0),
-    minWidth: 120,
-  },
-  selectEmpty: {
-    marginTop: theme.spacing(2),
-  },
-}));
-function App() {
-  const classes = useStyles();
-  const [age, setAge] = React.useState('');
+function App(){
+  return(
 
-  const handleChange = (event) => {
-    setAge(event.target.value);
-    console.log(age)
-  };
-  return (
-    <>
-     <FormControl className={classes.formControl}>
-        <InputLabel id="demo-simple-select-label">Age</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={age}
-          onChange={handleChange}
-        >
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-      </FormControl>
-    </>
+    <h1>Hello</h1>
   );
 }
-
 export default App;

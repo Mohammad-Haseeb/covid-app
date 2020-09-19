8601fdbe4faac61b71d68bc5049a2c2215612a20
Muhaamd-Haseeb <64950545+Muhaamd-Haseeb@users.noreply.github.com>
2020-09-19 17:34:37 +0500

    Simple Search bar Add

--- src/App.js
+++ src/App.js
@@ -1,40 +1,10 @@
-import React,{useEffect,useState} from 'react';
-import TotalCases from './Components/TotalCasesDisplay';
-import './App.css'
-import SimpleSelect from './Components/SearchBar';
+import React from 'react';
+import SelectBar from './Components/Selectbar';
 
-function App(){
-  const [state, setstate] = useState([])
-         useEffect(()=>{
-                  async function GetingData(){
-                      let apiCalling4AlCountries=await fetch("https://disease.sh/v3/covid-19/countries");
-                       let Data4Countries=await apiCalling4AlCountries.json();
-                         let completeCountryData=Data4Countries.map((countryName)=>({
-                          name :  countryName.country,
-                          value : countryName.countryInfo.iso2,
-                         }));
-                         setstate(completeCountryData);
-
-
-                   }
-                   GetingData();
-         },[])
-
-
-
-
-
-
+export default function App(){
   return(
-    <> 
-          <div className="SimpleSearch">
-              <SimpleSelect countryData={state}/>
-          </div>
-    <TotalCases /> 
-
+    <>
+    <SelectBar/>
     </>
-
-        
-     );
+  )
 }
-export default App;
